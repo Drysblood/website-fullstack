fb9ebfd0dc5bddc1479c4ce064210555e20f7dc0
Dry <140325871+Drysblood@users.noreply.github.com>
2025-06-30 09:38:06 +0200

    Delete src directory

--- src/app/services/dashboard-data.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class DashboardDataService {
-  newsList: { text: string; category: string }[] = [];
-  projects: { title: string; description?: string }[] = [];
-  serviceList: { title: string }[] = [];
-  kontaktList: { title: string }[] = [];
-}
